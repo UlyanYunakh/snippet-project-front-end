f1184c87bd632da566a306597224033dea1ec8db
UlyanYunakh <ulyanyunakh@gmail.com>
2021-07-28 12:40:15 +0300

    Add new components to AppModule.

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FeedComponent } from './feed/feed.component';
+import { TagsComponent } from './tags/tags.component';
+import { LangsComponent } from './langs/langs.component';
+import { SearchComponent } from './search/search.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FeedComponent,
+    TagsComponent,
+    LangsComponent,
+    SearchComponent,
+    ProfileComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
