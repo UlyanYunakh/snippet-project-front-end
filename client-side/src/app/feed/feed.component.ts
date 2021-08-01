df325a655c5ea6c4c0d984df0745b124761dd775
UlyanYunakh <ulyanyunakh@gmail.com>
2021-08-01 21:07:01 +0300

    Add feed component.

--- client-side/src/app/feed/feed.component.ts
+++ client-side/src/app/feed/feed.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { SortOption } from '../models/SortOption';
 
 @Component({
-  selector: 'app-feed',
-  templateUrl: './feed.component.html',
-  styleUrls: ['./feed.component.css']
+    selector: 'app-feed',
+    templateUrl: './feed.component.html',
+    styleUrls: ['./feed.component.css']
 })
 export class FeedComponent implements OnInit {
+    public sortOption: SortOption = { fieldName: "date", sortDirection: 0 };
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    setSortOption(option: SortOption) {
+        this.sortOption = option;
+    }
+
+    ngOnInit(): void {
+
+    }
 
 }
