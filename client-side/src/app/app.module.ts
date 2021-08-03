3eb2c0da0c4edf5780c4124f4bec831d4d8ce5ca
UlyanYunakh <ulyanyunakh@gmail.com>
2021-08-03 17:40:23 +0300

    Update refs to snippet list component.

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -25,7 +25,7 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { CreateComponent } from './components/create/create.component';
 import { TopLangsComponent } from './top-langs/top-langs.component';
 import { TopTagsComponent } from './top-tags/top-tags.component';
-import { SnippetsListComponent } from './snippets-list/snippets-list.component';
+import { SnippetsListComponent } from './components/snippets-list/snippets-list.component';
 import { ShortSnippetComponent } from './components/short-snippet/short-snippet.component';
 import { SnippetComponent } from './components/snippet/snippet.component';
 import { LangComponent } from './components/lang/lang.component';
