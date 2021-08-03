1868c3ce6a61439505d27ea4ef3a4a2d97825937
UlyanYunakh <ulyanyunakh@gmail.com>
2021-08-03 17:41:10 +0300

    Move sort component to components.

--- client-side/src/app/components/sort/sort.component.ts
+++ client-side/src/app/components/sort/sort.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { SortLink } from '../models/SortLink';
+import { SortLink } from '../../models/SortLink';
 
 @Component({
   selector: 'app-sort',
