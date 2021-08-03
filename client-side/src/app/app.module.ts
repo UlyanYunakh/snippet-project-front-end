0952f18c39af99201eeee11195f0d461d6c7be48
UlyanYunakh <ulyanyunakh@gmail.com>
2021-08-03 17:45:10 +0300

    Update refs to top tags component.

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -24,7 +24,7 @@ import { SearchComponent } from './components/search/search.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { CreateComponent } from './components/create/create.component';
 import { TopLangsComponent } from './components/top-langs/top-langs.component';
-import { TopTagsComponent } from './top-tags/top-tags.component';
+import { TopTagsComponent } from './components/top-tags/top-tags.component';
 import { SnippetsListComponent } from './components/snippets-list/snippets-list.component';
 import { ShortSnippetComponent } from './components/short-snippet/short-snippet.component';
 import { SnippetComponent } from './components/snippet/snippet.component';
