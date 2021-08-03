305d7a3c1e5a06cdebd175d853144a75795d6ac2
UlyanYunakh <ulyanyunakh@gmail.com>
2021-08-03 17:31:43 +0300

    Move feed component to components.

--- client-side/src/app/components/feed/feed.component.ts
+++ client-side/src/app/components/feed/feed.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { SortLink } from '../models/SortLink';
+import { SortLink } from '../../models/SortLink';
 
 @Component({
     selector: 'app-feed',
