5f96aa4cab3e8c01f5e2584ace5dde554231181c
UlyanYunakh <ulyanyunakh@gmail.com>
2021-08-03 17:38:41 +0300

    Update refs to short snippet component.

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -26,7 +26,7 @@ import { CreateComponent } from './components/create/create.component';
 import { TopLangsComponent } from './top-langs/top-langs.component';
 import { TopTagsComponent } from './top-tags/top-tags.component';
 import { SnippetsListComponent } from './snippets-list/snippets-list.component';
-import { ShortSnippetComponent } from './short-snippet/short-snippet.component';
+import { ShortSnippetComponent } from './components/short-snippet/short-snippet.component';
 import { SnippetComponent } from './snippet/snippet.component';
 import { LangComponent } from './components/lang/lang.component';
 import { SortComponent } from './sort/sort.component';
