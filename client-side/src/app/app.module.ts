5932fb6e3b6fe18d560a637be2ec4462531968e3
UlyanYunakh <ulyanyunakh@gmail.com>
2021-08-03 17:44:30 +0300

    Update refs to top langs component.

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -23,7 +23,7 @@ import { LangsComponent } from './components/langs/langs.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { CreateComponent } from './components/create/create.component';
-import { TopLangsComponent } from './top-langs/top-langs.component';
+import { TopLangsComponent } from './components/top-langs/top-langs.component';
 import { TopTagsComponent } from './top-tags/top-tags.component';
 import { SnippetsListComponent } from './components/snippets-list/snippets-list.component';
 import { ShortSnippetComponent } from './components/short-snippet/short-snippet.component';
