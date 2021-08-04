33eb034911d2f5dfc03d70f79c7a9c82943652e9
UlyanYunakh <ulyanyunakh@gmail.com>
2021-08-04 19:57:33 +0300

    Now snippet service extends base service.

--- client-side/src/app/services/snippet.service.ts
+++ client-side/src/app/services/snippet.service.ts
@@ -1,34 +1,25 @@
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable, throwError } from "rxjs";
-import { catchError, map, retry } from "rxjs/operators";
-import { environment } from "src/environments/environment";
 import { Snippet } from "../models/Snippet";
+import { BaseService } from "./base.service";
 
 @Injectable()
-export class SnippetService {
-    private url = environment.urlApi + "snippet/";
+export class SnippetService extends BaseService<Snippet> {
+    protected path = "snippet";
 
-    constructor(private http: HttpClient) { }
-
-    public get(id: string): Observable<Snippet> {
-        return this.http.get(this.url + id, { responseType: "text" }).pipe(
-            map(responce => {
-                return JSON.parse(responce, (key, value) => {
-                    if (key === "date") {
-                        return new Date(value);
-                    }
-                    else {
-                        return value;
-                    }
-                });
-            }),
-            retry(3),
-            catchError(this.HandleError)
-        );
+    constructor(http: HttpClient) {
+        super(http);
     }
 
-    private HandleError(error: HttpErrorResponse) {
-        return throwError("Ошибка! Попробуйте повторить попытку позже.");
+    protected getMap(responce: string) {
+        return JSON.parse(responce, (key, value) => {
+
+            if (key === "date") {
+                return new Date(value);
+            }
+            else {
+                return value;
+            }
+        });
     }
 }
