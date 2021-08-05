6dac40f87f5b951c674fec3cb4b5474bffd3b5e5
UlyanYunakh <ulyanyunakh@gmail.com>
2021-08-05 17:14:05 +0300

    Add getByName to lang service.

--- client-side/src/app/services/lang.service.ts
+++ client-side/src/app/services/lang.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { catchError, map, retry } from "rxjs/operators";
 import { Lang } from "../models/Lang";
 import { BaseService } from "./base.service";
 
@@ -7,7 +9,15 @@ import { BaseService } from "./base.service";
 export class LangService extends BaseService<Lang> {
     protected path = "lang";
 
-    constructor(http: HttpClient){
+    constructor(http: HttpClient) {
         super(http);
+    }
+
+    public getByName(name: string): Observable<Lang> {
+        return this.http.get(`${this.url}/${this.path}/by-name/${name}`, { responseType: "text" }).pipe(
+            map(responce => this.getMap(responce)),
+            retry(3),
+            catchError(this.HandleError)
+        );
     }
 }
