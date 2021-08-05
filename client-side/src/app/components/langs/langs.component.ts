bd424a59165196e8f772bb942391af474b7c7182
UlyanYunakh <ulyanyunakh@gmail.com>
2021-08-05 15:32:31 +0300

    Add lang service to langs component.

--- client-side/src/app/components/langs/langs.component.ts
+++ client-side/src/app/components/langs/langs.component.ts
@@ -1,34 +1,70 @@
+import { HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { LangService } from 'src/app/services/lang.service';
 import { Lang } from '../../models/Lang';
 
 @Component({
     selector: 'app-langs',
     templateUrl: './langs.component.html',
-    styleUrls: ['./langs.component.css']
+    providers: [LangService]
 })
 export class LangsComponent implements OnInit {
     public langs: Lang[] = [];
 
-    constructor() { }
+    public isReady = false;
+    public isErrorOccured = false;
+    public errorMessage!: string;
+
+    private currPage = 1;
+    
+    private httpParams!: HttpParams;
+
+    constructor(
+        private service: LangService,
+        private route: ActivatedRoute
+    ) { }
 
     ngOnInit(): void {
-        this.langs = [
-            {
-                id: 1,
-                name: "lang 1"
+        this.route.paramMap.subscribe((params: ParamMap) => {
+            this.langs = [];
+            this.setHttpParams(params);
+            this.getLangs();
+        });
+    }
+
+    private setHttpParams(params: ParamMap) {
+        var paramsObject: { [key: string]: any } = {
+            page: this.currPage,
+            pageSize: 20
+        };
+
+        if (params.get('sortOption')) {
+            paramsObject.sortOption = params.get('sortOption');
+        }
+        else {
+            paramsObject.sortOption = "abc";
+        }
+
+        this.httpParams = new HttpParams({
+            fromObject: paramsObject
+        });
+    }
+
+    public getLangs() {
+        this.isErrorOccured = false;
+
+        this.service.getMany(this.httpParams).subscribe(
+            responce => {
+                this.langs = this.langs.concat(responce);
+                this.httpParams = this.httpParams.set("page", ++this.currPage);
+                this.isReady = true;
             },
-            {
-                id: 2,
-                name: "lang 2"
-            },
-            {
-                id: 3,
-                name: "lang 3"
-            },
-            {
-                id: 4,
-                name: "lang 4"
+            error => {
+                this.errorMessage = error;
+                this.isErrorOccured = true;
+                this.isReady = false;
             }
-        ];
+        );
     }
 }
