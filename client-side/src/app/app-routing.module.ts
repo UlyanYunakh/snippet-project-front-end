e83b080d2ace99f53292609d91904345687c88a7
UlyanYunakh <ulyanyunakh@gmail.com>
2021-08-06 01:50:40 +0300

    Update routing.

--- client-side/src/app/app-routing.module.ts
+++ client-side/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateComponent } from './components/create/create.component';
 import { FeedLangComponent } from './components/feed-lang/feed-lang.component';
+import { FeedTagsComponent } from './components/feed-tags/feed-tags.component';
 import { FeedComponent } from './components/feed/feed.component';
 import { LangsComponent } from './components/langs/langs.component';
 import { ProfileComponent } from './components/profile/profile.component';
@@ -12,14 +13,15 @@ import { TagsComponent } from './components/tags/tags.component';
 const routes: Routes = [
     { path: 'feed/:sortOption', component: FeedComponent },
 
-    { path: 'langs/sort/:sortOption', component: LangsComponent },
     { path: 'langs/:langName/sort/:sortOption', component: FeedLangComponent },
+    { path: 'langs/sort/:sortOption', component: LangsComponent },
     { path: 'langs/:langName/:snippetId', component: SnippetComponent },
     { path: 'langs/:langName', component: FeedLangComponent },
     { path: 'langs', component: LangsComponent },
 
-    { path: 'tags/:tagName/sort/:sortOption', component: FeedComponent },
-    { path: 'tags/:tagName', component: FeedComponent },
+    { path: 'tags/:tagName/sort/:sortOption', component: FeedTagsComponent },
+    { path: 'tags/sort/:sortOption', component: TagsComponent },
+    { path: 'tags/:tagName', component: FeedTagsComponent },
     { path: 'tags', component: TagsComponent },
 
     { path: 'search', component: SearchComponent },
