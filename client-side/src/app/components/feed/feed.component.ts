eaa5e763667c566e1253801484946e362f1ff1b2
UlyanYunakh <ulyanyunakh@gmail.com>
2021-08-05 11:24:40 +0300

    Update feed component.

--- client-side/src/app/components/feed/feed.component.ts
+++ client-side/src/app/components/feed/feed.component.ts
@@ -1,19 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { SortLink } from '../../models/SortLink';
 
 @Component({
     selector: 'app-feed',
-    templateUrl: './feed.component.html',
-    styleUrls: ['./feed.component.css']
+    templateUrl: './feed.component.html'
 })
-export class FeedComponent implements OnInit {
+export class FeedComponent {
     public links: SortLink[] | undefined;
-
-    public sortOption!: string;
-    public langName = "";
-    public tagName = "";
-
 
     constructor(private route: ActivatedRoute) {
         this.links = [
@@ -27,13 +21,4 @@ export class FeedComponent implements OnInit {
             }
         ];
     }
-
-    ngOnInit(): void {
-        this.route.paramMap.subscribe((params: ParamMap) => {
-            this.sortOption = params.get('sortOption') ?? '';
-            this.langName = params.get('langName') ?? '';
-            this.tagName = params.get('tagName') ?? '';
-        });
-    }
-
 }
