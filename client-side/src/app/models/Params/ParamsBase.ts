3e68bbd24178aa82d2436593e9f2f6acd4bf36d6
UlyanYunakh <ulyanyunakh@gmail.com>
2021-08-05 11:31:30 +0300

    Remove params class.

--- client-side/src/app/models/Params/ParamsBase.ts
+++
@@ -1,9 +0,0 @@
-import { OrderDirection } from "../Enums/OrderDirection";
-
-export class ParamsBase {
-    public page: number = 0;
-    public pageSize: number = 20;
-    
-    public orderBy: string = "date";
-    public orderDirection: OrderDirection = 1;
-}
