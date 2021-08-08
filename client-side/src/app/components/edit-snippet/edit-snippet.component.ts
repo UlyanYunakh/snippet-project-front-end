a328d9611d884c39b71c0eb257c797c5110e2481
UlyanYunakh <ulyanyunakh@gmail.com>
2021-08-08 19:08:16 +0300

    Add getSnippetToEdit funct to edit component.

--- client-side/src/app/components/edit-snippet/edit-snippet.component.ts
+++ client-side/src/app/components/edit-snippet/edit-snippet.component.ts
@@ -1,6 +1,7 @@
 import { HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Lang } from 'src/app/models/Lang';
 import { Snippet } from 'src/app/models/Snippet';
 import { LangService } from 'src/app/services/lang.service';
@@ -14,14 +15,18 @@ import { SnippetService } from 'src/app/services/snippet.service';
 export class EditSnippetComponent implements OnInit {
     public form!: FormGroup;
     public langs!: Lang[];
+    public snippet!: Snippet;
 
     public isReady = false;
     public isErrorOccured = false;
     public errorMessage!: string;
 
+    private snippetId!: string;
+
     constructor(
         private langService: LangService,
-        private snippetService: SnippetService
+        private snippetService: SnippetService,
+        private route: ActivatedRoute
     ) {
         this.form = new FormGroup({
             "languageId": new FormControl("", [
@@ -42,7 +47,44 @@ export class EditSnippetComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.getLangs();
+        this.route.paramMap.subscribe((params: ParamMap) => {
+            if (params.get("snippetId")) {
+                this.snippetId = params.get("snippetId")!;
+                this.getSnippetToEdit();
+            }
+            this.getLangs();
+        });
+    }
+
+    public getSnippetToEdit() {
+        this.isErrorOccured = false;
+
+        this.snippetService.get(this.snippetId).subscribe(
+            responce => {
+                this.snippet = responce;
+
+                this.form = new FormGroup({
+                    "languageId": new FormControl(this.snippet.language.id, [
+                        Validators.required
+                    ]),
+                    "title": new FormControl(this.snippet.title, [
+                        Validators.required,
+                        Validators.maxLength(140)
+                    ]),
+                    "description": new FormControl(this.snippet.description, [
+                        Validators.maxLength(2000)
+                    ]),
+                    "snippet": new FormControl(this.snippet.snippet, [
+                        Validators.required,
+                        Validators.maxLength(4000)
+                    ])
+                });
+            },
+            error => {
+                this.errorMessage = error;
+                this.isErrorOccured = true;
+            }
+        );
     }
 
     public getLangs() {
