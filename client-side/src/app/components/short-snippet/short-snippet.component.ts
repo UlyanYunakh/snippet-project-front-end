e21829781c8cc9a13f80e427263426557224653f
UlyanYunakh <ulyanyunakh@gmail.com>
2021-08-10 17:38:36 +0300

    Move link to snippet title.

--- client-side/src/app/components/short-snippet/short-snippet.component.ts
+++ client-side/src/app/components/short-snippet/short-snippet.component.ts
@@ -1,16 +1,10 @@
-import { Component, Inject, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ShortSnippet } from '../../models/ShortSnippet';
 
 @Component({
     selector: 'app-short-snippet',
     templateUrl: './short-snippet.component.html'
 })
-export class ShortSnippetComponent implements OnInit {
+export class ShortSnippetComponent {
     @Input() snippet!: ShortSnippet;
-
-    constructor() { }
-
-    ngOnInit(): void {
-    }
-
 }
