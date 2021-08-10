086d338033ad036a7ebbca0397c7cc3a1a01edf8
UlyanYunakh <ulyanyunakh@gmail.com>
2021-08-10 18:54:11 +0300

    Add copyUrl option.

--- client-side/src/app/components/short-snippet/short-snippet.component.ts
+++ client-side/src/app/components/short-snippet/short-snippet.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ShortSnippet } from '../../models/ShortSnippet';
+import { ClipboardService } from 'ngx-clipboard';
 
 @Component({
     selector: 'app-short-snippet',
@@ -7,4 +8,14 @@ import { ShortSnippet } from '../../models/ShortSnippet';
 })
 export class ShortSnippetComponent {
     @Input() snippet!: ShortSnippet;
+
+    private baseURL = location.origin;
+
+    constructor(
+        private clipboardApi: ClipboardService
+    ) { }
+
+    copyUrl(langName:string, snippetId: number) {
+        this.clipboardApi.copyFromContent(`${this.baseURL}/langs/${langName}/${snippetId}`);
+    }
 }
