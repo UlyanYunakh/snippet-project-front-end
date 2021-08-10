b811aa93309d75d0ffd3726ec40c83c82193d46a
Ulyan Yunakh <44743429+UlyanYunakh@users.noreply.github.com>
2021-08-10 09:45:26 +0300

    Search component (#8)
    * Remove style from searcg component.
    
    * Seqrchin by langs and langs exclude options.
    
    * Add langs error handling.
    
    * Add tags and tags exclude options to search.
    
    * Add creation date option to form group.
    
    * Add form and to option to search.
    
    * Add validation message to creation date.
    
    * Add match string option to search.
    
    * Fix grammar. :)

--- client-side/src/app/components/search/search.component.ts
+++ client-side/src/app/components/search/search.component.ts
@@ -1,15 +1,165 @@
+import { HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Lang } from 'src/app/models/Lang';
+import { ShortSnippet } from 'src/app/models/ShortSnippet';
+import { LangService } from 'src/app/services/lang.service';
+import { SnippetShortService } from 'src/app/services/snippet-short.service';
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+    selector: 'app-search',
+    templateUrl: './search.component.html',
+    providers: [LangService, SnippetShortService]
 })
 export class SearchComponent implements OnInit {
+    public form: FormGroup | undefined;
+    public langs: Lang[] | undefined;
+    public shortSnippets: ShortSnippet[] | undefined;
+    public errorMessage: string | undefined;
+    public submittingState = false;
 
-  constructor() { }
+    constructor(
+        private langService: LangService,
+        private snippetService: SnippetShortService
+    ) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.setup();
+    }
 
+    public setup() {
+        this.errorMessage = undefined;
+        this.getLangs();
+        this.createBasicForm();
+    }
+
+    public submit() {
+        this.submittingState = true;
+        this.getSnippets();
+    }
+
+    public addLangToArray() {
+        (<FormArray>this.form!.controls["langs"]).push(new FormControl("", Validators.required));
+    }
+
+    public removeLangFromArray(index: number) {
+        (<FormArray>this.form!.controls["langs"]).removeAt(index);
+    }
+
+    public getLangsFormsControls(): FormArray {
+        return this.form!.controls["langs"] as FormArray;
+    }
+
+    public addExcludeLangToArray() {
+        (<FormArray>this.form!.controls["langsExclude"]).push(new FormControl("", Validators.required));
+    }
+
+    public removeExcludeLangFromArray(index: number) {
+        (<FormArray>this.form!.controls["langsExclude"]).removeAt(index);
+    }
+
+    public getExcludeLangsFormsControls(): FormArray {
+        return this.form!.controls["langsExclude"] as FormArray;
+    }
+
+    public addTagToArray() {
+        (<FormArray>this.form!.controls["tags"]).push(new FormControl("", Validators.required));
+    }
+
+    public removeTagFromArray(index: number) {
+        (<FormArray>this.form!.controls["tags"]).removeAt(index);
+    }
+
+    public getTagsFormsControls(): FormArray {
+        return this.form!.controls["tags"] as FormArray;
+    }
+
+    public addExcludeTagToArray() {
+        (<FormArray>this.form!.controls["tagsExclude"]).push(new FormControl("", Validators.required));
+    }
+
+    public removeExcludeTagFromArray(index: number) {
+        (<FormArray>this.form!.controls["tagsExclude"]).removeAt(index);
+    }
+
+    public getExcludeTagsFormsControls(): FormArray {
+        return this.form!.controls["tagsExclude"] as FormArray;
+    }
+
+    public addCreationDateToGroup() {
+        this.removeFromDateFromGroup();
+        this.removeToDateFromGroup();
+        this.form?.addControl("creationDate", new FormControl("", Validators.required));
+    }
+
+    public removeCreationDateFromGroup() {
+        this.form?.removeControl("creationDate");
+    }
+
+    public addFromDateToGroup() {
+        this.removeCreationDateFromGroup();
+        this.form?.addControl("from", new FormControl("", Validators.required));
+    }
+
+    public removeFromDateFromGroup() {
+        this.form?.removeControl("from");
+    }
+
+    public addToDateToGroup() {
+        this.removeCreationDateFromGroup();
+        this.form?.addControl("to", new FormControl("", Validators.required));
+    }
+    
+    public removeToDateFromGroup() {
+        this.form?.removeControl("to");
+    }
+    
+    public addMatchStringToGroup() {
+        this.form?.addControl("matchString", new FormControl("", Validators.required));
+    }
+
+    public removeMatchStringFromGroup() {
+        this.form?.removeControl("matchString");
+    }
+
+    private createBasicForm() {
+        this.form = new FormGroup({
+            "langs": new FormArray([]),
+            "langsExclude": new FormArray([]),
+            "tags": new FormArray([]),
+            "tagsExclude": new FormArray([]),
+            "page": new FormControl(1),
+            "pageSize": new FormControl(100)
+        });
+    }
+
+    private getSnippets() {
+        this.snippetService.getMany(this.form?.getRawValue()).subscribe(
+            responce => {
+                this.shortSnippets = responce;
+                this.submittingState = false;
+            },
+            error => {
+                this.errorMessage = "Не удалось найти сниппеты, подходящие под критерии поиска."
+                this.submittingState = false;
+            }
+        );
+    }
+
+    private getLangs() {
+        this.langService.getMany(new HttpParams({
+            fromObject: {
+                page: 1,
+                pageSize: 100,
+                sortOption: "popular"
+            }
+        })).subscribe(
+            responce => {
+                this.langs = responce;
+            },
+            error => {
+                this.errorMessage = "Не удалось загрузить языки.";
+            }
+        );
+    }
 }
