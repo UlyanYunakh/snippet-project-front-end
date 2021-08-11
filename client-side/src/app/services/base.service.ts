386b82b091f3195ff7054a79c2367cf2367316e9
UlyanYunakh <ulyanyunakh@gmail.com>
2021-08-11 22:54:46 +0300

    Catching auth error.

--- client-side/src/app/services/base.service.ts
+++ client-side/src/app/services/base.service.ts
@@ -34,11 +34,15 @@ export abstract class BaseService<T> {
     }
 
     protected HandleError(error: HttpErrorResponse) {
+        switch(error.message){
+            case "Login required":
+                return throwError("Нет доступа. Убедитесь, что вы авторизованы.");
+        }
         switch (error.status) {
             case 400:
                 return throwError("Неверные данные. Убедитесь, что данные введены верно.");
             case 401:
-                return throwError("Нет доступа. Убедитесь, что вы авторизованы.");
+                return throwError("Сервер отказал вам в доступе.");
             case 404:
                 return throwError("Не удалось найти подходящий ресурс.");
             default:
