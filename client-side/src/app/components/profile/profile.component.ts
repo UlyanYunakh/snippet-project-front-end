824ce8841ff541a423f97f18776668203ec9f08a
UlyanYunakh <ulyanyunakh@gmail.com>
2021-08-11 12:28:32 +0300

    Remove style from profile component.

--- client-side/src/app/components/profile/profile.component.ts
+++ client-side/src/app/components/profile/profile.component.ts
@@ -2,28 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+    selector: 'app-profile',
+    templateUrl: './profile.component.html'
 })
 export class ProfileComponent implements OnInit {
 
-  profileJson : string | null = null;
+    profileJson: string | null = null;
 
-  constructor(public auth: AuthService ) { }
+    constructor(public auth: AuthService) { }
 
-  ngOnInit(): void {
-    this.auth.user$.subscribe(
-      profile =>{
-        this.profileJson = JSON.stringify(profile, null, 2);
-        console.log(this.profileJson);
-      }
-    )
-  }
+    ngOnInit(): void {
+        this.auth.user$.subscribe(
+            profile => {
+                this.profileJson = JSON.stringify(profile, null, 2);
+                console.log(this.profileJson);
+            }
+        )
+    }
 
-  isAuthenticated()
-  {
+    isAuthenticated() {
 
-  }
+    }
 
 }
